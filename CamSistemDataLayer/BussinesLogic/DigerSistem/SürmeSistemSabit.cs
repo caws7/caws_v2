a18d6e9b8e918ec77d63c2af6d6f5c3cc396a1ff
Cafer BABACAN <mirketailesi@gmail.com>
2026-05-26 23:20:49 +0300

    1

--- CamSistemDataLayer/BussinesLogic/DigerSistem/SürmeSistemSabit.cs
+++ CamSistemDataLayer/BussinesLogic/DigerSistem/SürmeSistemSabit.cs
@@ -5,30 +5,67 @@ namespace CamSistemDataLayer.BussinesLogic.DigerSistem
 {
     public static class SürmeSistemSabit
     {
-        public static List<Profil> profilKesimOlcusuHesaplama(int en, int boy, int adet, List<Profil> profilList)
+        public static List<Profil> profilKesimOlcusuHesaplama(
+            int en,
+            int yukseklik,
+            int kanatAdedi,
+            int sistemAdedi,
+            List<Profil> profilList)
         {
             List<Profil> newProfilList = new List<Profil>();
             if (profilList == null) return newProfilList;
+
+            int toplamKanatAdedi = kanatAdedi * sistemAdedi;
 
             foreach (Profil item in profilList)
             {
                 switch ((item?.ProfilKodu ?? "").Trim())
                 {
                     // Profil kesim şablon alanı:
-                    // Aşağıdaki case bloklarını profil kodlarına göre çoğaltıp
-                    // kesim ölçüsü / adet formüllerini doldurabilirsiniz.
+                    // Burada her profil için ölçü hangi girdiye bağlıysa ona göre hesap yazılmalı.
+                    // toplam adet hesabında:
+                    // - kanat bazlı parçalarda toplamKanatAdedi
+                    // - sistem bazlı parçalarda sistemAdedi
+                    // kullanılabilir.
+
+
+                    // Örnek: sistem başına 1 kez kullanılan profil
+                    case "KAR-4326":
+                        item.KesimOlcusu = en- 32;
+                        item.KesimAdet = sistemAdedi * 2;
+                        item.ToplamAgirlik = profilToplamAgirlikHesaplama(
+                            item.KesimOlcusu,
+                            item.KesimAdet,
+                            (item.BirimAgirlik ?? 0));
+                        break;
+
+                    case "KAR-4325":
+                        item.KesimOlcusu = yukseklik - 5;
+                        item.KesimAdet = sistemAdedi * 2;
+                        item.ToplamAgirlik = profilToplamAgirlikHesaplama(
+                            item.KesimOlcusu,
+                            item.KesimAdet,
+                            (item.BirimAgirlik ?? 0));
+                        break;
 
                     case "SURME-PROFIL-1":
                         item.KesimOlcusu = en;
-                        item.KesimAdet = adet;
-                        item.ToplamAgirlik = profilToplamAgirlikHesaplama(item.KesimOlcusu, item.KesimAdet, (item.BirimAgirlik ?? 0));
+                        item.KesimAdet = toplamKanatAdedi;
+                        item.ToplamAgirlik = profilToplamAgirlikHesaplama(
+                            item.KesimOlcusu,
+                            item.KesimAdet,
+                            (item.BirimAgirlik ?? 0));
                         break;
 
                     case "SURME-PROFIL-2":
-                        item.KesimOlcusu = boy;
-                        item.KesimAdet = adet;
-                        item.ToplamAgirlik = profilToplamAgirlikHesaplama(item.KesimOlcusu, item.KesimAdet, (item.BirimAgirlik ?? 0));
+                        item.KesimOlcusu = yukseklik;
+                        item.KesimAdet = toplamKanatAdedi;
+                        item.ToplamAgirlik = profilToplamAgirlikHesaplama(
+                            item.KesimOlcusu,
+                            item.KesimAdet,
+                            (item.BirimAgirlik ?? 0));
                         break;
+
                 }
 
                 newProfilList.Add(item);
@@ -40,26 +77,41 @@ namespace CamSistemDataLayer.BussinesLogic.DigerSistem
         private static double profilToplamAgirlikHesaplama(int olcu, int adet, int agirlik)
             => olcu * adet * ((double)agirlik / 1000) / 1000;
 
-        public static List<CamBilgileri> CamYukseklikHesapla(int boy, int en, int adet)
+        public static List<CamBilgileri> CamYukseklikHesapla(
+            int en,
+            int yukseklik,
+            int kanatAdedi,
+            int sistemAdedi)
         {
             List<CamBilgileri> camEntityList = new List<CamBilgileri>();
 
-            // Cam hesap şablon alanı:
-            // Aşağıdaki ölçü formüllerini sürme sistem profillerine göre güncelleyebilirsiniz.
-            double yukseklik = boy;
-            int genislik = en;
+            int toplamKanatAdedi = kanatAdedi * sistemAdedi;
 
-            camEntityList.Add(CreateCamBilgisi("KAYAR CAM", genislik, (int)yukseklik, adet));
-            camEntityList.Add(CreateCamBilgisi("ORTA CAM", genislik, (int)yukseklik, adet));
-            camEntityList.Add(CreateCamBilgisi("SABİT CAM", genislik, (int)yukseklik, adet));
+            // Cam hesap şablon alanı:
+            // Nihai formüller profile göre burada düzenlenecek.
+            double camYukseklik = yukseklik;
+            int camGenislik = en;
+
+            camEntityList.Add(CreateCamBilgisi("KAYAR CAM", camGenislik, (int)camYukseklik, toplamKanatAdedi));
+            camEntityList.Add(CreateCamBilgisi("ORTA CAM", camGenislik, (int)camYukseklik, toplamKanatAdedi));
+            camEntityList.Add(CreateCamBilgisi("SABİT CAM", camGenislik, (int)camYukseklik, toplamKanatAdedi));
 
             return camEntityList;
         }
 
-        public static List<Profil> DigerMalzemeHesaplama(int en, int boy, int adet, List<Profil> profilList)
+        public static List<Profil> DigerMalzemeHesaplama(
+            int en,
+            int yukseklik,
+            int kanatAdedi,
+            int sistemAdedi,
+            List<Profil> profilList)
         {
             // Aksesuar / diğer malzeme şablon alanı:
-            // Gerekirse profilList içinden ilgili kodlara göre ayrı hesaplar burada yapılabilir.
+            // Örn:
+            // - kanat başına kullanılan malzeme => kanatAdedi * sistemAdedi
+            // - sistem başına kullanılan malzeme => sistemAdedi
+            // - çevre ölçüsüne bağlı malzeme => en / yukseklik ile hesaplanır
+
             return profilList ?? new List<Profil>();
         }
 
